f19eec5ddf508d91f32a45819a3d1c2f465b9d12
yuting.li@rograndec.com <yuting.li@rograndec.com>
2017-11-04 11:21:36 +0800

    update pom

--- src/main/java/com/springboot/demo/controller/view/IndexController.java
+++ src/main/java/com/springboot/demo/controller/view/IndexController.java
@@ -3,33 +3,34 @@ package com.springboot.demo.controller.view;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 	
-	@RequestMapping("/")
+	@RequestMapping(value="/",method = RequestMethod.GET)
     public String goindex(){
         return "index";
     }
 	
-	@RequestMapping("/index")
+	@RequestMapping(value="/index",method = RequestMethod.GET)
     public String index(){
         return "index";
     }
 	
 	@PreAuthorize("hasRole('user')")
-    @RequestMapping(value = "/admin")
+    @RequestMapping(value = "/admin",method = RequestMethod.GET)
     public String toAdmin(){
         return "user/admin";
     }
 	
 	@PreAuthorize("hasRole('user')")
-	@RequestMapping("/user")
+	@RequestMapping(value="/user",method = RequestMethod.GET)
     public String user(){
         return "user/user";
     }
 	
-	@RequestMapping("/403")
+	@RequestMapping(value="/403",method = RequestMethod.GET)
     public String error(){
         return "403";
     }
--- src/main/java/com/springboot/demo/controller/view/LoginController.java
+++ src/main/java/com/springboot/demo/controller/view/LoginController.java
@@ -1,9 +1,8 @@
 package com.springboot.demo.controller.view;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * 
  * @ClassName: LoginController   
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 	
 	//private Logger logger = LoggerFactory.getLogger(LoginController.class);
-	@RequestMapping("/login")
+	@RequestMapping(value="/login",method = RequestMethod.GET)
     public String login(){
         return "login/login";
     }
